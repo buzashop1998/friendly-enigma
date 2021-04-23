8ec19fe53dfde6f52f48b00d0488e55e985972f0
angelakki <angelakki@outlook.com>
2021-04-23 11:47:27 -0700

    make it better

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
-	$config["sql_host"] = "localhost";
-	$config["sql_user"] = "root";
-	$config["sql_pass"] = "";
-	$config["db_name"] = "db";
+	$config["sql_host"] = "us-cdbr-east-03.cleardb.com";
+	$config["sql_user"] = "b334460956e9d9";
+	$config["sql_pass"] = "e9628fe3";
+	$config["db_name"] = "heroku_b0b5a4cd593418f";
 ?>
