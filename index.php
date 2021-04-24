5bfc8a6c4837ebb3b725263bcb52c9dce6d4f1af
MOSETI <enock.g.moseti@gmail.com>
2021-04-24 15:53:34 +0300

    a

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 set_time_limit(0);
 session_start();
-require_once '/lib/Twig/Autoloader.php';
+require_once 'lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('template');
 $twig = new Twig_Environment($loader, [
